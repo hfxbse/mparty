f0fefebed185ff6d7b120750c641bea6c18f5a71
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-05-24 19:16:06 +0200

    Changed json_loader to use assert statements for the errorhandling
    Changed json_loader to use assert statements for the errorhandling

--- scripts/data_provider/json_loader.gd
+++ scripts/data_provider/json_loader.gd
@@ -5,12 +5,4 @@ func load_json_file(file_path: String):
 		var data_file = FileAccess.open(file_path, FileAccess.READ)
 		assert(data_file != null, "Couldn't read file.")
 		var parsed_data = JSON.parse_string(data_file.get_as_text())
-		
-		if parsed_data is Dictionary:
-			return parsed_data
-		else:
-			print("Couldn't read file.")
-			return null
-	else:
-		print("File doesn't exists.")
-		return null
+		assert(parsed_data is Dictionary, "Coudln't parse file.")
