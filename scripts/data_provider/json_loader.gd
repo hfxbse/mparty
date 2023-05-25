bcf5ddc4013c3c0be344e999549499a2232dd927
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-05-25 08:11:25 +0200

    Made requested changes, mainly using a difficulty mapping now.

--- scripts/data_provider/json_loader.gd
+++ scripts/data_provider/json_loader.gd
@@ -1,8 +1,8 @@
 class_name JSONLoader extends Resource
 
 func load_json_file(file_path: String):
-	if FileAccess.file_exists(file_path):
-		var data_file = FileAccess.open(file_path, FileAccess.READ)
-		assert(data_file != null, "Couldn't read file.")
-		var parsed_data = JSON.parse_string(data_file.get_as_text())
-		assert(parsed_data is Dictionary, "Coudln't parse file.")
+	var data_file = FileAccess.open(file_path, FileAccess.READ)
+	assert(data_file != null, "Couldn't read file at \"%s\"." % file_path)
+	var parsed_data = JSON.parse_string(data_file.get_as_text())
+	assert(parsed_data is Dictionary, "Coudln't parse file.")
+	return parsed_data
--- scripts/data_provider/quesions_provider.gd
+++ scripts/data_provider/quesions_provider.gd
@@ -2,6 +2,11 @@ class_name QuestionsProvider extends JSONLoader
 
 var file_path = "res://scripts/data/questions.json"
 enum {EASY, NORMAL, DIFFICULT}
+const difficulty_mapping = {
+	"0": EASY,
+	"1": NORMAL,
+	"2": DIFFICULT
+}
 var questions = {EASY: [], NORMAL: [], DIFFICULT: []}
 var current_question_index = -1;
 
@@ -12,23 +17,18 @@ func _init():
 		var question_obj = Question.new()
 		question_obj.question = question["question"]
 		
-		for answer_possibility in question["answer_possibilities"]:
-			question_obj.answer_possibilities.append(answer_possibility)
+		for answer_possiblity in question["answer_possibilities"]:
+			question_obj.answer_possibilities.append(answer_possiblity)
 			
 		question_obj.right_answer = question["right_answer"]
 		
-		var difficulty = question["difficulty"]
+		var difficulty = difficulty_mapping[str(question["difficulty"])]
+		assert(difficulty != null, "Difficulty \"%s\" is not supported." % difficulty)
 		question_obj.difficulty = (difficulty)
-		
-		if(difficulty == 0):
-			questions[EASY].append(question_obj)
-		elif(difficulty == 1):
-			questions[NORMAL].append(question_obj)
-		elif(difficulty == 2):
-			questions[DIFFICULT].append(question_obj)
+		questions[difficulty].append(question_obj)
 		
 func get_random_question(difficulty: int):
-	return questions.get(difficulty)[randi() % questions[difficulty].size()].get_question()
+	return questions[difficulty][randi() % questions[difficulty].size()].get_question()
 
 func get_question(index: int, difficulty: int):
 	return questions[difficulty][index]
@@ -39,5 +39,5 @@ func get_next_question(difficulty: int):
 	else: 
 		current_question_index = 0
 	var question = get_question(current_question_index, difficulty)
-	return question
 	assert(question != null, "No question found.")
+	return question
