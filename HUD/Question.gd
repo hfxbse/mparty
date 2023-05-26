5b54a4c46a335adf2a551b0eb36f11763ff1f2c8
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-05-26 19:50:20 +0200

    FirstHudPush
    First push, Idear of how the hud could work/look

---
+++ HUD/Question.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+func _on_answer_a_pressed():
+	var label = $Panel/Label
+	var button = $Panel/Button
+	label.set_text(String("Your response is A"))
+
+
+func _on_answer_b_pressed():
+	var label = $Panel/Label
+	var button = $Panel/Button
+	label.set_text(String("Your response is B"))
+
+
+func _on_answer_c_pressed():
+	var label = $Panel/Label
+	var button = $Panel/Button
+	label.set_text(String("Your response is C"))
+
+
+func _on_answer_d_pressed():
+	var label = $Panel/Label
+	var button = $Panel/Button
+	label.set_text(String("Your response is D"))
