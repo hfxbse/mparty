a921fc1ac3bf12759e608f1d2d6e21e512b9ba03
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-05-28 12:52:48 +0200

    Add QuestionProvider

---
+++ scripts/main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _init():
+	var question_handler = preload("res://resources/question_provider.tres")
+	#Example call
+	print(question_handler.get_next_question(Question.Difficulty.EASY).question)
+	print(question_handler.get_random_question(Question.Difficulty.EASY).question)
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
