d3d01303a7c2f774b0c6f9d9a7e6a9e5caeacfa5
Felix Burkard <felix-burkard@web.de>
2023-06-02 11:22:02 +0200

    Adapting Files to new Structure

--- main/main.gd
+++ main/main.gd
@@ -1,14 +1,21 @@
 extends Node
 
+var player
+var map
+
 func _init():
 	var question_handler = preload("res://resources/question_provider.tres")
 	#Example call
 	print(question_handler.get_next_question(Question.Difficulty.EASY).question)
 	print(question_handler.get_random_question(Question.Difficulty.EASY).question)
-
+	
 func _ready():
-	pass
+	player = $Player
+	map = $Map
+	player.currentLocation = map.get_start()
 
+func _on_button_pressed():
+	map.move("/root/Node/Player", 2)
 
 func _process(delta):
 	pass
