778444a055f08c4e73334ed8fe69c87f6b6db5b2
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-06-14 16:37:23 +0200

    Update DicePanel.gd
    push dice

--- HUD/DicePanel.gd
+++ HUD/DicePanel.gd
@@ -1,10 +1,10 @@
 extends Panel
 
 func _on_button_pressed():
-	$Panel/AnimationPlayer.play("popUpScale")
-	var label = $Panel/Label
-	var button = $Panel/Button
+	$AnimationPlayer.play("popUpScale")
+	var label = $Label
+	var button = $Button
 	var random = randi_range(1,6)
 	label.set_text(String("%d" % random))
-	$Panel/AnimationPlayer.play("popUpUniversal")
+	$AnimationPlayer.play("popUpUniversal")
 	button.hide()
