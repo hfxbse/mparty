25da1f09e09a4f00fa6caeab859faaa76491b3b5
Felix Burkard <felix-burkard@web.de>
2023-06-16 22:30:35 +0200

    Cleaned up splitField.gd

--- map/splitField.gd
+++ map/splitField.gd
@@ -4,7 +4,7 @@ extends "res://map/field.gd"
 
 signal path_picked
 signal path2_picked
-signal step_ended
+signal step_completed
 
 var path_ended
 var playerPath
@@ -15,25 +15,25 @@ func move(player: NodePath):
 	path2_picked.connect(_on_path2_picked)
 	path.target.button.activate(path_picked)
 	path2.target.button.activate(path2_picked)
-	return step_ended
+	return step_completed
 
-func _on_path_picked():
+func _cleanUp():
 	path2_picked.disconnect(_on_path2_picked)	
 	path_picked.disconnect(_on_path_picked)
 	path.target.button.deactivate()
 	path2.target.button.deactivate()
+
+func _on_path_picked():
+	_cleanUp()
 	path_ended = path.move(playerPath)
 	path_ended.connect(_on_path_ended)
 	
 	
 func _on_path2_picked():
-	path2_picked.disconnect(_on_path2_picked)
-	path_picked.disconnect(_on_path_picked)
-	path2.target.button.deactivate()
-	path.target.button.deactivate()
+	_cleanUp()
 	path_ended = path2.move(playerPath)
 	path_ended.connect(_on_path_ended)
 	
 func _on_path_ended():
 	path_ended.disconnect(_on_path_ended)
-	step_ended.emit()
+	step_completed.emit()
