e4dcc451ffaec6f2ffce94e0b8157ab96eabc96e
Fabian Haas <inf22008@lehre.dhbw-stuttgart.de>
2023-06-20 20:16:39 +0200

    dynamically determin field of split field
    this allows the split field to extend any field type

--- map/fields/split_field/split_field.gd
+++ map/fields/split_field/split_field.gd
@@ -1,6 +1,6 @@
 class_name SplitField extends Node2D
 
-@onready var field = $Field
+var field
 @onready var path2 = $Path2
 
 signal path_picked
@@ -9,6 +9,27 @@ signal step_completed
 
 var path_ended
 var playerPath
+
+const missing_field_warning = "SplitField needs a Field as child node."
+const too_many_fields_warining = "SplitField cannot have more than one Field as child."
+
+
+func get_fields():
+	var fields = []
+	
+	for child in get_children():
+		if child is Field:
+			fields.append(child)
+
+	return fields
+
+
+func _ready():
+	var fields = get_fields()
+	assert(!fields.is_empty(), missing_field_warning)
+	assert(fields.size() == 1, too_many_fields_warining)
+
+	field = fields[0]
 
 
 func enable_picker():
