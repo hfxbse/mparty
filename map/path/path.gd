4b3e0a29b85f546718336a93714c6d006d54761b
Fabian Haas <inf22008@lehre.dhbw-stuttgart.de>
2023-06-22 01:27:13 +0200

    add path editor warnings
    also rename Path to MapPath to be have less confusion with Path2D.

--- map/path/path.gd
+++ map/path/path.gd
@@ -1,14 +1,46 @@
-class_name Path extends Path2D
+@tool
+class_name MapPath extends Path2D
 
 signal field_reached
-@export var targetPath : NodePath
 
-@onready var target = get_node(targetPath)
+var target
+@export var target_field: NodePath:
+	set(path):
+		if path != target_field:
+			target_field = path
+			if Engine.is_editor_hint(): update_configuration_warnings()
+
+
 @onready var follow = $Follow
 
+const target_not_set = "Path target not set."
+const target_type_mismatch = "Path target needs to be of type Field."
+
+
+func _ready():
+	if target_field.is_empty():
+		target = null
+	else:
+		target = get_node(target_field)
+
+	if not Engine.is_editor_hint():
+		assert(target != null, target_not_set)
+	else:
+		update_configuration_warnings()
+
+
+func _get_configuration_warnings():
+	var errors = []
+
+	if target_field.is_empty(): errors.append(target_not_set)
+
+	return errors
+
+
 func _process(delta):
-	if !follow.remote.update_position:
-		return
+	if Engine.is_editor_hint(): return
+
+	if !follow.remote.update_position: return
 
 	follow.progress_ratio +=  delta * 1.5
 
@@ -27,4 +59,3 @@ func move(player: NodePath):
 	get_node(player).currentLocation = target
 
 	return field_reached
-
