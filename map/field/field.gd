58b033e3e813e9fb5e42d9079ac835fb4d367d0b
Fabian <29468630+hfxbse@users.noreply.github.com>
2023-06-26 22:52:26 +0200

    Implement field color overrides
    PR #18

--- map/field/field.gd
+++ map/field/field.gd
@@ -10,12 +10,54 @@ var picked_receivers: Array[Callable]
 var field_reached: Signal
 
 
+var shadow_color_overrides: Dictionary = {
+	"disabled": func (): return theme_override.field_color,
+	"normal": func (): return theme_override.shadow_color_normal,
+	"hover": func (): return theme_override.shadow_color_normal,
+	"pressed": func (): return theme_override.shadow_color_pressed,
+}
+
+
+@export var theme_override: FieldTheme:
+	set(theme):
+		if theme != theme_override:
+			if theme_override != null:
+				theme_override.changed.disconnect(apply_theme_overide)
+			if theme != null: 
+				theme.changed.connect(apply_theme_overide)
+
+			theme_override = theme
+			apply_theme_overide()
+
+
+func apply_theme_overide():
+	for style_name in theme.get_stylebox_list("Button"):
+		if theme_override == null:
+			remove_theme_stylebox_override(style_name)
+			continue
+
+		var style: StyleBoxFlat = theme.get_stylebox(style_name, "Button").duplicate()
+		style.bg_color = theme_override.field_color
+
+		var shadow_color = shadow_color_overrides.get(style_name)
+		if shadow_color != null: style.shadow_color = shadow_color.call()
+
+		add_theme_stylebox_override(style_name, style)
+
+
 func _ready():
 	paths = []
+	var child_theme_override: FieldTheme = null
 
 	for child in get_children():
 		if child is MapPath:
 			paths.append(child)
+		
+		if "field_theme_override" in child && child.field_theme_override != null:
+			child_theme_override = child.field_theme_override
+
+	if theme_override == null:
+		theme_override = child_theme_override
 
 	if not Engine.is_editor_hint():
 		assert(not paths.is_empty(), missing_path_warning)
