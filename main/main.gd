69ebe5b429583a7f461cd272e78046106c97402a
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-07-02 16:24:18 +0200

    clear branch
    clear branch for starting signals between notification and actions

--- main/main.gd
+++ main/main.gd
@@ -10,24 +10,6 @@ func _ready():
 	player = $Player
 	map = $Map
 	player.currentLocation = map.get_start()
-	var question_difficulty_menu = preload("res://fieldActions/questionDifficulty/questionDifficulty.tscn").instantiate()
-	_add_question_difficulty_menu(question_difficulty_menu)
-
-
-func _add_question_difficulty_menu(question_difficulty_menu: Control):
-	add_child(question_difficulty_menu)
-	question_difficulty_menu.selected_difficulty.connect()
-	
-func _remove_question_difficulty_menu():
-	for child in get_children():
-		if child.name == "questionDifficulty":
-			remove_child(child)
-
-func _on_on_selected_difficulty(difficulty: int):
-	_remove_question_difficulty_menu()
-	#Aufruf QuestionMenu analog wie Aufruf DifficultyMenu
-	#Methode von dem QuestioMenu aufrufen und difficulty übergeben
-	pass
 
 func _on_button_pressed():
 	$Button.hide()
