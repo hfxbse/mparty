15e81e57fa98650e037fae97f3038fb2d3403570
Felix Burkard <felix-burkard@web.de>
2023-07-03 17:45:13 +0200

    Added Structure and Signals for Main-Loop to Main

--- main/main.gd
+++ main/main.gd
@@ -1,35 +1,48 @@
 extends Node
 
-@onready var player: Player = $Player
-@onready var map = $Map
-@onready var MoveButton = $MoveButton
+## HUD Elements can attach here to show current Player
+signal player_turn_begin(player)
+## HUD Elements can attach here to show current Round
+signal round_begin(roundNum)
 
+@onready var start : Field = map.start
+@onready var currentPlayer: Player = $Player
+@onready var map = $Map
+
+var players
 
 func _init():
 	var question_handler = preload("res://data_providers/resources/question_provider.tres")
 	#Example call
 	print(question_handler.get_next_question(Question.Difficulty.EASY).question)
 	print(question_handler.get_random_question(Question.Difficulty.EASY).question)
+	
+func startGame(numRounds, numPlayers):
+## Is called by the start-screen to start the game
+	createPlayers(numPlayers)
+	## orderPlayers(numPlayers)
+	for round in numRounds:
+		round_begin.emit(round+1)
+		for player in players:
+			playerTurn(player)
+	
+func createPlayers(num):
+## Allocate the correct number of players
+## set their currentLocation and global_position
+## initialize "Riesen" and "Patente"
+	pass
+	
+func orderPlayers(numPlayers):
+## Get order of players by rolling dice
+## Order Players Accordingly
+	pass
 
-
-func _ready():
-	var start = map.start
-	player.current_location = start
-	player.global_position = start.global_position
-
-
-func _on_button_pressed():
-	MoveButton.hide()
-	player.move(2)
-
-
-func _on_player_turn_ended():
-	MoveButton.show()
-
-
-func _on_back_button_pressed():
-	player.move_backwards(1)
-
-
-func _on_undo_button_pressed():
-	player.undo_last_move()
+func rollDice():
+## roll dice and return the eyecount
+	pass
+	
+func playerTurn(player):
+	currentPlayer = player
+	player_turn_begin.emit(currentPlayer)
+	var diceRoll = await rollDice()
+	await player.move(diceRoll)
