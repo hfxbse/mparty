38a2b5895fd05bfa35310032c6da12b7a434be4d
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-07-05 16:50:07 +0200

    Improve drag movement

--- main/MainCamera.gd
+++ main/MainCamera.gd
@@ -2,10 +2,10 @@ extends Camera2D
 
 var last_pos
 var target_zoom = 1.0
+var zoom_rate = 5.0
 const MIN_ZOOM = 0.2
 const MAX_ZOOM = 2.0
 const ZOOM_INCREMENT = 0.1
-const ZOOM_RATE = 5.0
 
 func _ready():
 	last_pos = position
@@ -14,7 +14,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if is_current():  
 		if event is InputEventMouseMotion:
 			if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
-				position -= event.relative * zoom
+				position -= event.relative
 		elif event is InputEventMouseButton:
 			if event.is_pressed:
 				if event.button_index == MOUSE_BUTTON_WHEEL_UP:
@@ -33,7 +33,7 @@ func zoom_out():
 func _physics_process(delta):
 	zoom = lerp(zoom, 
 	target_zoom * Vector2.ONE,
-	ZOOM_RATE * delta
+	zoom_rate * delta
 	)
 	set_physics_process(
 		not is_equal_approx(zoom.x, target_zoom)
