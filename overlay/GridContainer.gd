b17cdc68c86935935be4f55e3c1bced268c3f575
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-07-05 21:32:45 +0200

    add base overlay
    old branch is corrputed, new one here

---
+++ overlay/GridContainer.gd
@@ -0,0 +1,18 @@
+extends GridContainer
+
+@onready var geld1 = $Geldstatus1
+@onready var geld2 = $Geldstatus2
+@onready var geld3 = $Geldstatus3
+@onready var geld4 = $Geldstatus4
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var spieler = $Spieler1 #change this based on playerturn
+	spieler.add_theme_color_override("font_color", Color(1, 0.5, 0))
+	
+	var money = "1" #how to change the Text of Moneystats
+	geld1.set_text(money)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
