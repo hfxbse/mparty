1012b609d6050af7d8b019006d44eef9218e04b2
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-07-12 13:15:42 +0200

    Add patent office

---
+++ patent_office/patent_office.gd
@@ -0,0 +1,24 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_difficulty_1_pressed():
+	#buy patent (communication with patent and money storage
+	var kost_of_patent = 10
+	var patent_value = 5
+	#stop move? --> has to be approved first
+	pass # Replace with function body.
+
+
+func _on_difficulty_2_pressed():
+	#just continue move
+	pass # Replace with function body.
