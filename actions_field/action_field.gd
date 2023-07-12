5eb40561bc85e5fbc6276ef47ef77084313132f2
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-07-12 13:19:24 +0200

    Add action_field

---
+++ actions_field/action_field.gd
@@ -0,0 +1,17 @@
+extends Panel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#movement operation, send movement
+	#dice event, allow dice operations
+	#patent operation or stable money operations, change value of patents, send varaibles to calculator...
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	self.visible = false
+	#go to dice
