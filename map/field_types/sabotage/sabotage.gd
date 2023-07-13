a10388229f8d8ae4e7c038ad2920325530804195
Felix Burkard <felix-burkard@web.de>
2023-07-13 21:57:16 +0200

    Connected sabotagte to field

--- map/field_types/sabotage/sabotage.gd
+++ map/field_types/sabotage/sabotage.gd
@@ -3,4 +3,6 @@ extends Node
 @export var field_theme_override : FieldTheme
 
 func event(player : Player):
-	print("Sabotage Triggered")
+	var sabotage_panel = preload("res://sabotage_field/sabotage_field.tscn").instantiate()
+	add_child(sabotage_panel)
+	await sabotage_panel.display(player)
--- sabotage_field/sabotage_field.gd
+++ sabotage_field/sabotage_field.gd
@@ -2,11 +2,14 @@ extends Control
 
 var player
 
+signal terminated
+
 func _ready():
-	self.visible = false
+	visible = false
 
 
 func _on_button_pressed():
+	visible = false
 	var dice_menu = preload("res://dice/dice.tscn").instantiate()
 	add_child(dice_menu)
 	var dice_value = await dice_menu.dice_number
@@ -16,9 +19,11 @@ func _on_button_pressed():
 	add_child(result_display)
 	await result_display.display(player, dice_value)
 	remove_child(result_display)
+	terminated.emit()
 
 
 func display(player: Player):
 	self.player = player
 	visible = true
+	return terminated
 	
