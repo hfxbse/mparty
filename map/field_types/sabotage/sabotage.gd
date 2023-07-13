ec2340782d78153f6970eee581c7e19fdbddd815
Felix Burkard <felix-burkard@web.de>
2023-07-13 22:03:12 +0200

    Added code for closing of sabotage dialogs

--- map/field_types/sabotage/sabotage.gd
+++ map/field_types/sabotage/sabotage.gd
@@ -6,3 +6,4 @@ func event(player : Player):
 	var sabotage_panel = preload("res://sabotage_field/sabotage_field.tscn").instantiate()
 	add_child(sabotage_panel)
 	await sabotage_panel.display(player)
+	remove_child(sabotage_panel)
