ba5038418556e8cf8ee05ebb7fd8953c49a6f464
Hamarius <hamaru@protonmail.com>
2023-07-15 14:21:12 +0200

    snake case for pictures and removing not needed functions in question_difficulty

--- question_difficulty/question_difficulty.gd
+++ question_difficulty/question_difficulty.gd
@@ -1,15 +1,11 @@
 extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
 
 signal difficulty(diff: Question.Difficulty)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_difficulty_1_pressed():
 	difficulty.emit(Question.Difficulty.EASY)
