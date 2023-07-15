10b92e0f8839027031c06274e482371b5d18f367
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-07-15 12:53:59 +0200

    Hot fix question_panel
    selcected_answer wasn't set anymore, because the answer text is now located in a label

--- question_panel/answer_button.gd
+++ question_panel/answer_button.gd
@@ -5,7 +5,6 @@ class_name AnswerButton extends Node
 signal button_pressed(answer_text: String)
 
 
-@onready var answer_button: Button = $Button
 @onready var answer_button_label = $Button/Label
 
 @export var button_text: String = "":
@@ -22,12 +21,12 @@ func _ready():
 
 
 func _on_button_pressed():
-	button_pressed.emit(answer_button.text)
+	button_pressed.emit(answer_button_label.text)
 	
 func _wrong_answer_selected():
-	answer_button.add_theme_color_override("font_color", Color(255,0,0))
+	answer_button_label.add_theme_color_override("font_color", Color(255,0,0))
 
 	
 func _right_answer_selected():
-	answer_button.add_theme_color_override("font_color", Color(0,128,0))
+	answer_button_label.add_theme_color_override("font_color", Color(0,128,0))
 	
