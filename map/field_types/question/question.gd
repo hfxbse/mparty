d1bc8d5aeccc4163b4d26b8fc3081f436d5a0332
Fabian Haas <inf22008@lehre.dhbw-stuttgart.de>
2023-07-16 12:32:01 +0200

    change file formating

--- map/field_types/question/question.gd
+++ map/field_types/question/question.gd
@@ -3,20 +3,17 @@ extends Node
 @export var field_theme_override : FieldTheme
 
 func event(player : Player):
-	var question_panel = preload("res://question_panel/question_panel.tscn").instantiate()
 	var question_difficulty = preload("res://question_difficulty/question_difficulty.tscn").instantiate()
 	add_child(question_difficulty)
+
 	var difficulty = await question_difficulty.difficulty
 	remove_child(question_difficulty)
-	
+
+	var question_panel = preload("res://question_panel/question_panel.tscn").instantiate()
 	add_child(question_panel)
-	
-	var result = await question_panel.display_question(difficulty)
-	if result:
+
+	if await question_panel.display_question(difficulty):
 		player.riesen += 20 * (difficulty + 1)
-		print("right answer")
-	else:
-		print("wrong answer")
-		
+
 	await get_tree().create_timer(1.5).timeout
 	remove_child(question_panel)
