41e6111d8a9bb6411e7288062fcc5ae5a2090499
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-07-16 16:41:55 +0200

    Add start menu
    PR #55 and PR #59
    Co-authored-by: KevinBasener <120453853+KevinBasener@users.noreply.github.com>

--- podium/podium.gd
+++ podium/podium.gd
@@ -6,8 +6,10 @@ extends CanvasLayer
 
 
 func display_winner(players_in_order: Array):
-	assert(players_in_order.size() >= 3, "Too few players to display winners.")
+	assert(players_in_order.size() >= 2, "Too few players to display winners.")
 	
 	first.texture = players_in_order[0].sprite.texture
 	second.texture = players_in_order[1].sprite.texture
-	third.texture = players_in_order[2].sprite.texture
+	
+	if players_in_order.size() > 2:
+		third.texture = players_in_order[2].sprite.texture
