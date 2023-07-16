1dcfe2673b777ed3719e98c10df19770f3580839
Felix Burkard <felix-burkard@web.de>
2023-07-16 16:30:24 +0200

    Sabotage dialog
    PR #35

---
+++ sabotage_dialog/sabotage_dialog.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+var player
+
+signal finished
+
+func _on_button_pressed():
+	visible = false
+	var dice_menu = preload("res://dice/dice.tscn").instantiate()
+	add_child(dice_menu)
+	var dice_value = await dice_menu.dice_number
+	remove_child(dice_menu)
+	
+	var result_display = preload("res://sabotage_dialog/sabotage_dice_result.tscn").instantiate()
+	add_child(result_display)
+	await result_display.display(player, dice_value)
+	remove_child(result_display)
+	finished.emit()
+
+
+func display(player: Player):
+	self.player = player
+	return finished
+	
