7dc9edcc7f6fe4d6e0d92dba535e5c7b482697be
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-07-16 13:33:42 +0200

    Add patent office
    PR #44 and #64
    Co-authored-by: KevinBasener <120453853+KevinBasener@users.noreply.github.com>
    Co-authored-by: Hamarius <hamaru@protonmail.com>
    Co-authored-by: Felix Burkard <felix-burkard@web.de>

--- map/field_types/patent_office/patent_office.gd
+++ map/field_types/patent_office/patent_office.gd
@@ -4,3 +4,8 @@ extends Node
 
 func event(player : Player):
 	print("Patent_Office Triggered")
+	var patent_office = preload("res://patent_office/patent_office.tscn").instantiate()
+	add_child(patent_office)
+	await patent_office.start_transaction(player)
+	remove_child(patent_office)
+	
