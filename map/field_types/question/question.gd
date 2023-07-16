01a5f374f4f40dd05e11fd1c76baec3f57e1c428
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2023-07-16 12:33:33 +0200

    Reward player for right answers
    PR #53

--- map/field_types/question/question.gd
+++ map/field_types/question/question.gd
@@ -3,19 +3,17 @@ extends Node
 @export var field_theme_override : FieldTheme
 
 func event(player : Player):
-	var question_panel = preload("res://question_panel/question_panel.tscn").instantiate()
 	var question_difficulty = preload("res://question_difficulty/question_difficulty.tscn").instantiate()
 	add_child(question_difficulty)
+
 	var difficulty = await question_difficulty.difficulty
 	remove_child(question_difficulty)
-	
+
+	var question_panel = preload("res://question_panel/question_panel.tscn").instantiate()
 	add_child(question_panel)
-	
-	var result = await question_panel.display_question(difficulty)
-	if result:
-		print("right answer")
-	else:
-		print("wrong answer")
-		
+
+	if await question_panel.display_question(difficulty):
+		player.riesen += 20 * (difficulty + 1)
+
 	await get_tree().create_timer(1.5).timeout
 	remove_child(question_panel)
